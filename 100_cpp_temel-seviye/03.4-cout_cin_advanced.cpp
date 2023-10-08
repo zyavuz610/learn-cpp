29c0110a06697e0cad1aa41f34b8a33f6fe2755b
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2023-10-08 19:12:45 +0300

    Update 03.4-cout_cin_advanced.cpp

--- 100_cpp_temel-seviye/03.4-cout_cin_advanced.cpp
+++ 100_cpp_temel-seviye/03.4-cout_cin_advanced.cpp
@@ -61,68 +61,7 @@ int main() {
   // The second statement, cin.ignore(100,'\n'); discards all of the remaining numbers in the first line.
   // The third statement cin>>b; stores 12 (from the next line) in b.
 */
-//-----------------------------------------------------
-/*
-  // cin.putback(ch);   1 karakter geri gider okur
-  // ch=cin.peek();     sıradaki karakteri okur ancak ilerlemez
-  char ch;
 
-	cout<<"Line 1: Enter a string: ";		//Line 1
-	cin.get(ch);					//Line 2
-	cout<<endl;						//Line 3
-	cout<<"Line 4: After first cin.get(ch); "
-	    <<"ch = "<<ch<<endl;			//Line 4
-  cin.get(ch);					//Line 5
-	cout<<"Line 6: After second cin.get(ch); " 
-	    <<"ch = "<<ch<<endl;			//Line 6
-
-	cin.putback(ch);					//Line 7
-	cin.get(ch);					//Line 8
-	cout<<"Line 9: After putback and then " 
-	    <<"cin.get(ch); ch = "<<ch<<endl;	//Line 9
-	
-	ch = cin.peek();					//Line 10
-	cout<<"Line 11: After cin.peek(); ch = "
-	    <<ch<<endl;					//Line 11
-  cin.get(ch);					//Line 12
-	cout<<"Line 13: After cin.get(ch); ch = "
-	    <<ch<<endl;					//Line 13
-*/
-/************
-ÇIKTI:
-  Line 1: Enter a string: abcd
-
-Line 4: After first cin.get(ch); ch = a
-Line 6: After second cin.get(ch); ch = b
-Line 9: After putback and then cin.get(ch); ch = b
-Line 11: After cin.peek(); ch = c
-Line 13: After cin.get(ch); ch = c
-*/
-//------------------------------------------------------------------ 
-/*
-  // input failure
-  int a = 10;					   	//Line 1
-   int b = 20;					   	//Line 2
-   int c = 30;					   	//Line 3
-   int d = 40;					   	//Line 4
-	
-   cout<<"Line 5: Enter four integers: ";   	//Line 5
-   cin>>a>>b>>c>>d;				  	//Line 6
-   cout<<endl;					  	//Line 7
-  cout<<"Line 8: The numbers you entered are:"
-  	 <<endl;				   		//Line 8
-   cout<<"Line 9: a = "<<a<<", b = "<<b		
-	 <<", c = "<<c<<", d = "<<d<<endl;	   	//Line 9
-*/
-
-/********  ÖRNEK GİRİŞ:
-Line 5: Enter four integers: 34 K 67 28
-
-Line 8: The numbers you entered are:
-Line 9: a = 34, b = 20 (0?), c = 30, d = 40
-
-*** hata oluştuğunda sonrakileri direk atlar.
-**********/   
 //-------------------------------------------------------------------
 
   // getline(cin,str);
