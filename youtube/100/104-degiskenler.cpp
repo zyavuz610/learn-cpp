7ecba9953d149e9c3c61919b0a99841abfe61fd1
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-03 23:47:51 +0300

    Create 104-degiskenler.cpp

---
+++ youtube/100/104-degiskenler.cpp
@@ -0,0 +1,47 @@
+// C++ 104 - Değişkenler ve Değişken İsimlendirme Kuralları (Variables)
+#include <iostream>
+using namespace std;
+
+/*
+Değişkenler, 
+  içeriği değiştirilebilen bellek bölgelerinin sembolik temsili
+*/
+
+int main() {
+  int sayi1;
+  sayi1 = 10;
+  cout << "Değişken İçeriği:";
+  cout << sayi1 << endl << "\n";
+  sayi1 = 20;
+  cout << "Değişken Yeni İçeriği:";
+  cout << sayi1;
+  
+  cout << endl;
+  
+  float sayi2;
+  sayi2 = 10.2;
+  cout << "Değişken İçeriği:";
+  cout << sayi2 << endl << "\n";
+  sayi2 = -21.14;
+  cout << "Değişken Yeni İçeriği:";
+  cout << sayi2;
+  /*Değişken İsimlendirme Kuralları
+      ingilizcedeki alfanumerik karakterler kullanılabilir. a-z,A-Z,0-9
+      _ kullanılabilir
+      değişken isminin ilk karakteri sayı olamaz
+      c++ için özel anlam ifade eden kelimeler değişken ismi olamaz (int, float, if, else, return, for, while)
+
+Örnek:
+ sayi1, OK
+  sayi_1, OK 
+  sayı 1, HATA
+  1sayi, HATA  
+  A, OK
+  a, OK
+  _sayi1, OK
+  _, OK
+  __sayi, OK
+  num__ber, OK
+  birinci_sayi
+*/
+}
