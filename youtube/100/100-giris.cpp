9d700a1455ab0fb5e42b24b6c7c807dc5b652d57
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-03 23:29:02 +0300

    Create 100-giris.cpp

---
+++ youtube/100/100-giris.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Merhaba herkese\n";
+  return 0;
+}
+
+/*
+C++ 100 - C++ ile Programlama ve Yol Haritası
+    1. Programlama (yazılım) nedir?
+      giriş olarak veri alan, veriler üzerinde çözme yönelik işlem yapan ve sonucu çıktı olarak üreten kodları yazma işidir.
+    2. girdi - işlem - çıktı
+    3. C++ yol haritası
+      temel C++ programlama (1xx)
+      nesne yönelimli programlama (2xx)
+      stadart template library ve ileri konular (3xx)
+    4. temel C++ programlama (1xx)
+      kurulum, kod yazılacak ortam
+      ilk program, Hello World, (söz dizimi ve basit kurallar)
+      değişkenler ve operatörler
+      kullanıcı ile etkileşim girdi, çıktı
+      koşul ifadeleri, if, else
+      döngü ifadeleri, for, while
+      diziler
+      fonksiyonlar ve modüler programlama
+      pointer kavramı
+      dosya işlemleri
+*/
