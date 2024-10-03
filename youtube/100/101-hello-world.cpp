4ac0783e67fcb1f3435bd1e1050facebcdec5c86
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-03 23:32:19 +0300

    .

---
+++ youtube/100/101-hello-world.cpp
@@ -0,0 +1,8 @@
+/*
+  İlk program
+*/
+#include <iostream>
+
+int main() {
+  std::cout << "Merhaba Dünya\n\n\n";
+}
---
+++ youtube/100/102-cout.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout << "Merhaba Dünya\n" 
+       << "Merhaba C++\n" 
+       << endl;
+}
