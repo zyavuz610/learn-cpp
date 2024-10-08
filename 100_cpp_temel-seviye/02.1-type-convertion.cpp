4739f17d601bc4af1f46633345335adae8837df8
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-08 15:27:51 +0300

    Create 02.1-type-convertion.cpp

---
+++ 100_cpp_temel-seviye/02.1-type-convertion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    */
+    double d = 3.14;
+    int i = (int)d;  // C-Style Cast
+    cout << "C-Style Cast ile double'dan int'e dönüştürülen değer: " << i << std::endl;
+    
+    return 0;
+}
