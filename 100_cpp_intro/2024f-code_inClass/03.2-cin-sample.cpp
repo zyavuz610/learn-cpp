5e65eb30e3defb43ca769982194686779b8d4f29
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-10 13:42:14 +0300

    .

--- 100_cpp_intro/2024f-code_inClass/03.2-cin-sample.cpp
+++ 100_cpp_intro/2024f-code_inClass/03.2-cin-sample.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    return 0;
+    string ad;
+    cout<<"Profil Adiniz:";
+    cin>>ad;
     while(true){
         char ch; int a;
-        cout<<"Karakter (çıkmak için 0): ";
+        cout<<"Karakter (cikmak icin 0): ";
         cin>>ch;
         a = static_cast<int>(ch);
         cout << ch << ": " << a << endl;
+        if (ch == '0') break;
     }
+    cout<<"Gule gule "<<ad<<endl;
+    return 0;
 }
