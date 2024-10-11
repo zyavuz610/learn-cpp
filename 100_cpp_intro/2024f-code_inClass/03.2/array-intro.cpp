63c79e136e7fbabe77682d50a9dad99f07b65166
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-11 14:38:47 +0300

    .

--- 100_cpp_intro/2024f-code_inClass/03.2/array-intro.cpp
+++ 100_cpp_intro/2024f-code_inClass/03.2/array-intro.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <iomanip>
+#define SIZE 5
 using namespace std; // cin, cout için
 
 int main(){
-    float asNotlar[5] = {20,55,86,98,74};
+    float asNotlar[SIZE] = {20,55,86,98,74};
+    float fnNotlar[SIZE] = {55,65,75,58,99};
+    string isimler[SIZE] = {
+        "zafer",
+        "yavuz",
+        "ahmet",
+        "elif",
+        "betul"
+    };
+    //for(int i=0;i<SIZE;i++){ cin>>asNotlar[i];}   // kullanıcıdan değer almak
+    cout<<setw(4)<<right<<"id";
+    cout<<setw(14)<<right<<"isim";
+    cout<<setw(8)<<right<<"asNot";
+    cout<<endl<<"-------------------------------------"<<endl;
+    for(int i=0;i<SIZE;i++){
+        cout<<setw(4)<<right<<i;
+        cout<<setw(14)<<right<<isimler[i];
+        cout<<setw(8)<<right<<asNotlar[i]<<endl;
+    }
+    cout << "C++ version: " << __cplusplus << endl;
     return 0;
 }
