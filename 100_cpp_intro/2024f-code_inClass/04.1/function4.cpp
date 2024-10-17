63e18914f4d3faee25ca81dfae0c5a521288bb86
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-17 15:02:58 +0300

    .

---
+++ 100_cpp_intro/2024f-code_inClass/04.1/function4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// void myfunc(void);
+// void myfunc(int,int);
+
+int myfunc(int a=1,int b=2){
+    int toplam = a+b;
+    return toplam;
+}
+
+int main(){
+    // bu değerleri kullanıcıdan gerektiğinde burada alabiliriz
+    int a=5,b=6,top;
+    top = myfunc(a);
+    cout<<"Toplam = "<<top<<endl;
+
+
+    a=10; b=16;
+    cout<<"Toplam = "<<myfunc()<<endl;
+    return 0;
+}
+
+//----------------------------------------------------
---
+++ 100_cpp_intro/2024f-code_inClass/04.1/function5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// void myfunc(void);
+// void myfunc(int,int);
+
+int myfunc(const int a=1,const int b=2){
+    //.....
+    //a = 20; b=30;
+    int toplam = a+b;
+    return toplam;
+}
+
+int main(){
+    // bu değerleri kullanıcıdan gerektiğinde burada alabiliriz
+    int a=5,b=6,top;
+    top = myfunc(a,b);
+    cout<<"Toplam1 = "<<top<<endl;
+
+
+    a=10; b=16;
+    cout<<"Toplam2 = "<<myfunc()<<endl;
+    return 0;
+}
+
+//----------------------------------------------------
