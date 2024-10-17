43000a37d77bba94e07acc08f76c482224750e5c
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-17 14:48:30 +0300

    Create function3.cpp

---
+++ 100_cpp_intro/2024f-code_inClass/04.1/function3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// void myfunc(void);
+// void myfunc(int,int);
+
+int myfunc(int a,int b){
+    int toplam = a+b;
+    return toplam;
+    // cout<<a<<" + "<<b<<" = "<<toplam<<endl;
+}
+
+int main(){
+    // bu değerleri kullanıcıdan gerektiğinde burada alabiliriz
+    int a=5,b=6,top;
+    top = myfunc(a,b);
+    cout<<a<<" + "<<b<<" = "<<top<<endl;
+
+
+    a=10; b=16;
+    cout<<a<<" + "<<b<<" = "<<myfunc(a,b)<<endl;
+    return 0;
+}
+
+//----------------------------------------------------
