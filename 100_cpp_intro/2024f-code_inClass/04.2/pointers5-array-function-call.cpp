2f2eaffd726699e2d2bc54472d527a3535ca032d
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-10-18 14:47:12 +0300

    .

--- 100_cpp_intro/2024f-code_inClass/04.2/pointers5-array-function-call.cpp
+++ 100_cpp_intro/2024f-code_inClass/04.2/pointers5-array-function-call.cpp
@@ -6,9 +6,25 @@ int len(char *a){
     while(a[i] != '\0') i++;
     return i;
 }
+int sum(int *a,int n){
+    int s=0;
+    for(int i=0;i<n;i++){
+        s += a[i];
+    }
+    return s;
+}
+void upper(char *a){
+    while(*a != '\0'){
+        if(*a >= 'a' && *a <= 'z')
+           *a -= ('a' - 'A'); 
+         a++;
+    }
+}
 
 int main(){
     char a[] = "zafer";
+    cout<<a<<":"<<len(a)<<endl;
+    upper(a);
     cout<<a<<":"<<len(a)<<endl;
     return 0;
 }
