0876d15c52bc34d8acd5ff886ec857bbf2f837c4
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-11-08 13:10:54 +0300

    .

---
+++ _2024/07.2/1-stack.cpp
@@ -0,0 +1,10 @@
+/*
+veri yaplarında çokça kullanılan bir yığın class ı yazalım,
+yığın veri yapısı LIFO (Last In First Out) mantığına göre çalışır.
+
+max kapasite 100 olup, push, pop, peek, isEmpty, isFull fonksiyonlarını içermelidir.
+peek fonksiyonu yığının en üstündeki elemanı döner. yığın eleman sayısı değişmez
+pop fonksiyonu yığının en üstündeki elemanı döner ve yığından çıkarır.
+...
+
+*/
