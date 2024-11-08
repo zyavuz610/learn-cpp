6a3d5bf923caa7acbb8163ef227dd49c3c569697
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-11-08 13:17:13 +0300

    .

---
+++ _2024/07.2/3-stack_destructor.cpp
@@ -0,0 +1,13 @@
+/*
+veri yaplarında çokça kullanılan bir yığın class ı yazalım,
+yığın veri yapısı LIFO (Last In First Out) mantığına göre çalışır.
+
+max kapasite 100 olup, push, pop, peek, isEmpty, isFull fonksiyonlarını içermelidir.
+peek fonksiyonu yığının en üstündeki elemanı döner. yığın eleman sayısı değişmez
+pop fonksiyonu yığının en üstündeki elemanı döner ve yığından çıkarır.
+...
+
+
+yıkıcı fonksiyonu kullanarak yığın class ını temizleyelim. delete ile dinamik bellek alanını temizleyelim.
+
+*/
---
+++ _2024/07.2/6-stack_copy-constructor.cpp
@@ -0,0 +1,14 @@
+/*
+veri yaplarında çokça kullanılan bir yığın class ı yazalım,
+yığın veri yapısı LIFO (Last In First Out) mantığına göre çalışır.
+
+max kapasite 100 olup, push, pop, peek, isEmpty, isFull fonksiyonlarını içermelidir.
+peek fonksiyonu yığının en üstündeki elemanı döner. yığın eleman sayısı değişmez
+pop fonksiyonu yığının en üstündeki elemanı döner ve yığından çıkarır.
+...
+
+
+stack değişkeninin kopyala, farklı bir stack oluyor mu?
+bu durumu sabit,dinamik diziler için de test edin.
+
+*/
