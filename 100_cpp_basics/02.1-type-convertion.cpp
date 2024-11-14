18977ed6745c71440dd8a91467e6e1fd117f98d6
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-11-14 16:54:41 +0300

    Update 02.1-type-convertion.cpp

--- 100_cpp_basics/02.1-type-convertion.cpp
+++ 100_cpp_basics/02.1-type-convertion.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main() {
     /*
+    tür dönüşümü (type conversion) ya da dönüşüm (conversion), bir veri türünü başka bir veri türüne dönüştürme işlemidir.
+    C++'ta tür dönüşümü 2 şekilde yapılır:
+    1. C-Style Cast: (int) x
+    2. C++-Style Cast: static_cast<int>(x)
+
+    veri kaybı olmadan dönüşüm yapılabilir, veri kaybı olabilecek dönüşümler için dönüşüm yapılırken dikkatli olunmalıdır.
+    küçük türden büyük türe dönüşümde veri kaybı olmaz, büyük türden küçük türe dönüşümde veri kaybı olabilir.
+    örneğin; 
+    int i = 10;
+    double d = (double)i;  // int'ten double'a dönüşümde veri kaybı olmaz
+
+    double d = 3.14;
+    int i = (int)d;  // double'dan int'e dönüşümde veri kaybı olabilir
+
+    bunların dışında tür dönüşümü otomatik olarak yapılır, örneğin;
+    int i = 10;
+    double d = i;  // int'ten double'a dönüşüm otomatik yapılır
+
     */
     double d = 3.14;
     int i = (int)d;  // C-Style Cast
