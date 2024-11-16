081070518eff5cba5df84412873462c48eaab7d6
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-11-16 11:51:47 +0300

    Create 141-fonk_tanimi_ve_cagirma.cpp

---
+++ youtube/100/141-fonk_tanimi_ve_cagirma.cpp
@@ -0,0 +1,28 @@
+/*
+Temel C++ programlama (1xx)
+  + kurulum, söz dizimi ve basit kurallar
+  + değişkenler ve operatörler
+  + kullanıcı ile etkileşim girdi, çıktı
+  + koşul ifadeleri, if, else
+  + döngü ifadeleri, for, while
+  + diziler
+  + 2D diziler ve iç içe döngüler, diğer koşul ifadeleri
+  - fonksiyonlar ve modüler programlama
+  pointer kavramı
+  dosya işlemleri
+*/
+#include <iostream>
+using namespace  std;
+
+//ret    name    par.
+  void fonksiyon(void){
+    // fonk. kodları buraya yazılır
+    cout<<"Merhaba Fonksiyonlar!"<<endl;
+  }
+
+int main() {
+  fonksiyon();
+  fonksiyon();
+  fonksiyon();
+  fonksiyon();
+}
