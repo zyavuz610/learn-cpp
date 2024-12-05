1865ae3599afdc360711f172227545be33f5178e
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-12-05 13:28:17 +0300

    Create time-class.cpp

---
+++ _2024/11.1/time-class.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+ 
+class Time {
+   private:
+      int hours;             // 0 to 23
+      int minutes;           // 0 to 59
+      
+   public:
+      // required constructors
+      Time() {
+         hours = 0;
+         minutes = 0;
+      }
+      Time(int h, int m) {
+         hours = h;
+         minutes = m;
+      }
+      
+      // method to display time
+      void displayTime() {
+         cout << "H: " << hours << " M:" << minutes <<endl;
+      }
+      
+      // overloaded prefix ++ operator, ++t
+      Time operator++ () {
+         ++minutes;          // increment this object
+         if(minutes >= 60) {
+            ++hours;
+            minutes -= 60;
+         }
+         return Time(hours, minutes);
+      }
+      
+      // overloaded postfix ++ operator
+      // t++;
+      Time operator++( int ) {
+      
+         // save the orignal value
+         Time T(hours, minutes);
+         
+         // increment this object
+         ++minutes;                    
+         
+         if(minutes >= 60) {
+            ++hours;
+            minutes -= 60;
+         }
+         // return old original value
+         return T; 
+      }
+};
+
+int main() {
+   Time T1(11, 59), T2(10,40);
+ 
+   ++T1;                    // increment T1
+   T1.displayTime();        // display T1
+   ++T1;                    // increment T1 again
+   T1.displayTime();        // display T1
+ 
+   T2++;                    // increment T2
+   T2.displayTime();        // display T2
+   T2++;                    // increment T2 again
+   T2.displayTime();        // display T2
+
+   if(T1<T2){
+        cout << "T1 is less than T2" << endl;
+   }
+   else{
+        cout << "T1 is greater than T2" << endl;
+   }
+   return 0;
+}
