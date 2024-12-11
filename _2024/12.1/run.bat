04b60ad43ad95b509355fd55db1a42b8ca8776e0
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-12-12 01:18:30 +0300

    ..

--- _2024/12.1/run.bat
+++ _2024/12.1/run.bat
@@ -1,6 +1,6 @@
 @echo off
 echo C++ kodu derleniyor...
-g++ shape-rect_inheritence.cpp -o program.exe
+g++ shape-rect_inheritance.cpp -o program.exe
 if %errorlevel% neq 0 (
     echo Derleme hatasi...
     pause
