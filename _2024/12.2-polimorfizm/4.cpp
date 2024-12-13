d4132b477ec1efa17510cd7955ad323d9926e5fc
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-12-13 14:42:54 +0300

    ..

---
+++ _2024/12.2-polimorfizm/4.cpp
@@ -0,0 +1,13 @@
+class Shape {
+   protected:
+      int width, height;
+      
+   public:
+      Shape( int a = 0, int b = 0){
+         width = a;
+         height = b;
+      }
+      virtual int area()=0;
+     
+    
+};
