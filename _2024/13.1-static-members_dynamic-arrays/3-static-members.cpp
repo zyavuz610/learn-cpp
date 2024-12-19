aaa86acf50472a6949124bf6b039b211e07220aa
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2024-12-19 14:03:56 +0300

    Update 3-static-members.cpp

--- _2024/13.1-static-members_dynamic-arrays/3-static-members.cpp
+++ _2024/13.1-static-members_dynamic-arrays/3-static-members.cpp
@@ -46,6 +46,7 @@ class Box {
 int Box::objectCount = 0;
 
 int main(void) {
+   cout << "Total objects (-1): " << Box::objectCount << endl;
    Box Box1(3.3, 1.2, 1.5);    // Declare box1
    Box Box2(8.5, 6.0, 2.0);    // Declare box2
    {
